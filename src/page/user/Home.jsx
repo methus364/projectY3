de6f21e0f6b1960fc18c7dda9b35b4c2449e3f59
methus364 <Agustinmathus3@gmail.com>
2025-06-30 09:05:23 +0700

    first commit

---
+++ src/page/user/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../../components/user/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Home
---
+++ src/routes/appRouter.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../page/user/Home";
+
+// รวม Router สำหรับเปลี่ยนไปหน้าต่างๆ
+const router = createBrowserRouter([
+  // {
+  //   // router ฝั่ง admin
+  //   path: "/admin",
+  //   element: <LayoutAdmin />,
+  //   children: [
+  //     { index: true, element: <Product /> },
+  //     { path: "coures", element: <Coures /> },
+  //     { path: "addproduct", element: <Addproduct /> },
+  //     { path: "addcoures", element: <Addcoures /> },
+  //     { path: "addcategory", element: <Addnewcategory /> },
+  //   ],
+  // },
+// router ฝั่ง user
+  {
+    path: "/",
+    children: [
+      { index: true, element: <Home/> },
+    ],
+  },
+]);
+
+const AppRouter = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default AppRouter;
