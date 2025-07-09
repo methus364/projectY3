2a703e1aaf2b7358936a35f8d0744a438335a106
PanuwatKidtook <sb6640248113@lru.ac.th>
2025-07-09 16:00:59 +0700

    test

---
+++ src/page/user/Booking.jsx
@@ -0,0 +1,13 @@
+import Carousel from '../../components/user/Carousel'
+import Navbar from '../../components/user/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Carousel/>
+    </>
+  )
+}
+
+export default Home
